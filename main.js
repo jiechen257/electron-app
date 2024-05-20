ff9ce28a53aaf761c14714e3c7b40c14c433e28b
jiechen <1487503910@qq.com>
2024-05-20 10:28:34 +0800

    feat: add preload script

--- main.js
+++ main.js
@@ -1,28 +1,29 @@
-const { app, BrowserWindow } = require("electron");
+const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("node:path");
 
 function createWindow() {
 	const win = new BrowserWindow({
 		width: 800,
 		height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, "preload.js"),
-    }
+		webPreferences: {
+			preload: path.join(__dirname, "preload.js"),
+		},
 	});
 
 	win.loadFile("index.html");
 }
 
-app.on("window-all-closed", () => {
-	if (process.platform !== "darwin") {
-		app.quit();
-	}
-});
-
 app.whenReady().then(() => {
+	ipcMain.handle('ping', () => 'pong');
 	createWindow();
 
 	app.on("activate", () => {
 		if (BrowserWindow.getAllWindows().length === 0) createWindow();
 	});
+});
+
+app.on("window-all-closed", () => {
+	if (process.platform !== "darwin") {
+		app.quit();
+	}
 });
--- preload.js
+++ preload.js
@@ -1,10 +1,9 @@
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector)
-    if (element) element.innerText = text
-  }
+const { contextBridge, ipcRenderer } = require('electron')
 
-  for (const type of ['chrome', 'node', 'electron']) {
-    replaceText(`${type}-version`, process.versions[type])
-  }
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+  // 除函数之外，我们也可以暴露变量
+  ping: () => ipcRenderer.invoke('ping')
 })
